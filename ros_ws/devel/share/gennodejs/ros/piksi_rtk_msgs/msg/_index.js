682bcbc01baec0571717e623cae7249012c3fc00
Otto Technogecko <ottotechnogecko@gmail.com>
2019-07-28 13:18:37 -0700

    just committing all the build kruft cause why not

---
+++ ros_ws/devel/share/gennodejs/ros/piksi_rtk_msgs/msg/_index.js
@@ -0,0 +1,74 @@
+
+"use strict";
+
+let Dops = require('./Dops.js');
+let ImuRawMulti = require('./ImuRawMulti.js');
+let DopsMulti = require('./DopsMulti.js');
+let TrackingState_V2_3_15 = require('./TrackingState_V2_3_15.js');
+let TrackingState = require('./TrackingState.js');
+let ReceiverState = require('./ReceiverState.js');
+let Heartbeat = require('./Heartbeat.js');
+let UtcTimeMulti = require('./UtcTimeMulti.js');
+let VelEcef = require('./VelEcef.js');
+let MagRaw = require('./MagRaw.js');
+let InfoWifiCorrections = require('./InfoWifiCorrections.js');
+let AgeOfCorrections = require('./AgeOfCorrections.js');
+let Observation = require('./Observation.js');
+let PosLlhMulti = require('./PosLlhMulti.js');
+let TrackingState_V2_2_15 = require('./TrackingState_V2_2_15.js');
+let PosLlh = require('./PosLlh.js');
+let PosEcef = require('./PosEcef.js');
+let UartState_V2_3_15 = require('./UartState_V2_3_15.js');
+let ImuAuxMulti = require('./ImuAuxMulti.js');
+let GpsTimeMulti = require('./GpsTimeMulti.js');
+let UartState = require('./UartState.js');
+let BaselineHeading = require('./BaselineHeading.js');
+let ReceiverState_V2_3_15 = require('./ReceiverState_V2_3_15.js');
+let Log = require('./Log.js');
+let ReceiverState_V2_4_1 = require('./ReceiverState_V2_4_1.js');
+let BasePosLlh = require('./BasePosLlh.js');
+let BasePosEcef = require('./BasePosEcef.js');
+let BaselineEcef = require('./BaselineEcef.js');
+let GpsTime = require('./GpsTime.js');
+let BaselineNed = require('./BaselineNed.js');
+let DeviceMonitor_V2_3_15 = require('./DeviceMonitor_V2_3_15.js');
+let VelNed = require('./VelNed.js');
+let MeasurementState_V2_4_1 = require('./MeasurementState_V2_4_1.js');
+let ReceiverState_V2_2_15 = require('./ReceiverState_V2_2_15.js');
+
+module.exports = {
+  Dops: Dops,
+  ImuRawMulti: ImuRawMulti,
+  DopsMulti: DopsMulti,
+  TrackingState_V2_3_15: TrackingState_V2_3_15,
+  TrackingState: TrackingState,
+  ReceiverState: ReceiverState,
+  Heartbeat: Heartbeat,
+  UtcTimeMulti: UtcTimeMulti,
+  VelEcef: VelEcef,
+  MagRaw: MagRaw,
+  InfoWifiCorrections: InfoWifiCorrections,
+  AgeOfCorrections: AgeOfCorrections,
+  Observation: Observation,
+  PosLlhMulti: PosLlhMulti,
+  TrackingState_V2_2_15: TrackingState_V2_2_15,
+  PosLlh: PosLlh,
+  PosEcef: PosEcef,
+  UartState_V2_3_15: UartState_V2_3_15,
+  ImuAuxMulti: ImuAuxMulti,
+  GpsTimeMulti: GpsTimeMulti,
+  UartState: UartState,
+  BaselineHeading: BaselineHeading,
+  ReceiverState_V2_3_15: ReceiverState_V2_3_15,
+  Log: Log,
+  ReceiverState_V2_4_1: ReceiverState_V2_4_1,
+  BasePosLlh: BasePosLlh,
+  BasePosEcef: BasePosEcef,
+  BaselineEcef: BaselineEcef,
+  GpsTime: GpsTime,
+  BaselineNed: BaselineNed,
+  DeviceMonitor_V2_3_15: DeviceMonitor_V2_3_15,
+  VelNed: VelNed,
+  MeasurementState_V2_4_1: MeasurementState_V2_4_1,
+  ReceiverState_V2_2_15: ReceiverState_V2_2_15,
+};
---
+++ ros_ws/devel/share/gennodejs/ros/piksi_rtk_msgs/srv/_index.js
@@ -0,0 +1,12 @@
+
+"use strict";
+
+let SettingsReadReq = require('./SettingsReadReq.js')
+let SettingsReadResp = require('./SettingsReadResp.js')
+let SettingsWrite = require('./SettingsWrite.js')
+
+module.exports = {
+  SettingsReadReq: SettingsReadReq,
+  SettingsReadResp: SettingsReadResp,
+  SettingsWrite: SettingsWrite,
+};
