4a09a3d6e4ca5c8e59a3611d06584af8d402455e
Yan Michalevsky <yanm2@cs.stanford.edu>
2019-08-03 13:07:51 -0700

    Added preprocess_kml.py

---
+++ ros_ws/src/frontend_server/preprocess_kml.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""
+This script expects a 'static' folder to be in the same directory
+and will download content into it.
+"""
+
+import sys
+import urllib.request
+from urllib.parse import urlparse
+import os
+from xml.dom import minidom
+
+BRCMAP_KML_FILE = 'brcmap.kml'
+CACHE_DIR = 'static'
+
+def eprint(*args, **kwargs):
+    "Print to standard error"
+    print(*args, file=sys.stderr, **kwargs)
+
+def cache_content(link):
+    "Cache content locally"
+    path = urlparse(link).path
+    dir = os.path.dirname(path)
+    if dir:
+        dir_to_create = CACHE_DIR + '/' + dir
+        if not os.path.exists(dir_to_create):
+            os.makedirs(dir_to_create)
+    urllib.request.urlretrieve(link, CACHE_DIR + path)
+
+def main():
+    kml_doc = minidom.parse(BRCMAP_KML_FILE)
+    links = kml_doc.getElementsByTagName('href')
+
+    for link in links:
+        originalLink = link.firstChild.data
+        # print('Original link:', originalLink)
+        try:
+            cache_content(originalLink)
+        except Exception as e:
+            eprint("Failed caching", originalLink, '(', e, ')')
+        # Update link in XML
+        link.firstChild.data = originalLink.replace('www.gstatic.com', 'localhost:9090')
+        # print('New link:', link.firstChild.data)
+
+    print(kml_doc.toxml())
+
+if __name__ == '__main__':
+    main()
